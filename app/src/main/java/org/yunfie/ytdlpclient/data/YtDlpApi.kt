62bc0d20d15c57949cd1f57ad51ebd45ad33bf54
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-07 15:41:57 +0900

    Add downloadFile endpoint to YtDlpApi

--- app/src/main/java/org/yunfie/ytdlpclient/data/YtDlpApi.kt
+++ app/src/main/java/org/yunfie/ytdlpclient/data/YtDlpApi.kt
@@ -1,9 +1,11 @@
 package org.yunfie.ytdlpclient.data
 
+import okhttp3.ResponseBody
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Streaming
 
 interface YtDlpApi {
     @POST("info")
@@ -17,4 +19,11 @@ interface YtDlpApi {
 
     @POST("download/cancel/{taskId}")
     suspend fun cancelDownload(@Path("taskId") taskId: String)
+
+    // Add endpoint to download the file from server
+    // Assumes server exposes files at /files/{filename}
+    // We use @Streaming to handle large files without loading into memory
+    @Streaming
+    @GET("files/{filename}")
+    suspend fun downloadFile(@Path("filename") filename: String): ResponseBody
 }
