8a50ad590ae07e3923e4b4f55445985ad579efe1
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-07 15:32:19 +0900

    Enable AppContent in MainActivity

--- app/src/main/java/org/yunfie/ytdlpclient/MainActivity.kt
+++ app/src/main/java/org/yunfie/ytdlpclient/MainActivity.kt
@@ -7,12 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import org.yunfie.ytdlpclient.ui.AppContent
 import org.yunfie.ytdlpclient.ui.theme.YtdlpClientTheme
-// import org.yunfie.ytdlpclient.ui.AppContent // Assuming this doesn't exist yet
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             YtdlpClientTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // AppContent(modifier = Modifier.padding(innerPadding))
-                    Text(
-                        text = "Hello Android!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppContent(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
