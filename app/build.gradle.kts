fdbf2dd77b4e6f775da71afa655271924342a93b
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-07 21:58:38 +0900

    Add documentfile dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.documentfile) // Added
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit)
     implementation(libs.retrofit.kotlinx.serialization)
