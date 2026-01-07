c75e527b127385db7c8371ebb790ca0ca2c7bbcc
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-07 21:54:49 +0900

    Add download location to SettingsRepository

--- app/src/main/java/org/yunfie/ytdlpclient/data/repository/SettingsRepository.kt
+++ app/src/main/java/org/yunfie/ytdlpclient/data/repository/SettingsRepository.kt
@@ -9,17 +9,29 @@ import org.yunfie.ytdlpclient.dataStore
 
 class SettingsRepository(private val context: Context) {
     companion object {
-        private val API_URL_KEY = stringPreferencesKey("api_url")
+        private val KEY_API_URL = stringPreferencesKey("api_url")
+        private val KEY_DOWNLOAD_LOCATION = stringPreferencesKey("download_location")
     }
 
     val apiUrl: Flow<String?> = context.dataStore.data
         .map { preferences ->
-            preferences[API_URL_KEY]
+            preferences[KEY_API_URL]
+        }
+        
+    val downloadLocation: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[KEY_DOWNLOAD_LOCATION]
         }
 
     suspend fun saveApiUrl(url: String) {
         context.dataStore.edit { preferences ->
-            preferences[API_URL_KEY] = url
+            preferences[KEY_API_URL] = url
+        }
+    }
+    
+    suspend fun saveDownloadLocation(uri: String) {
+        context.dataStore.edit { preferences ->
+            preferences[KEY_DOWNLOAD_LOCATION] = uri
         }
     }
 }
